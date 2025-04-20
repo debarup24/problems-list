c6cf7bf276559978d01e566af89cb95d2d3138e7
Debarup <87480898+debarup24@users.noreply.github.com>
2025-04-20 18:25:45 +0530

    check prime

---
+++ 5. Check Prime/checkPrime.cpp
@@ -0,0 +1,24 @@
+bool isPrime(int n)
+{
+    // Write your code here.
+    int count = 0;
+
+    for (int i = 1; i * i <= n; i++)
+    { // square root approach (better)
+        if (n % i == 0)
+        {
+            count++;
+
+            if (n / i != i)
+            { // check square root num
+                count++;
+            }
+        }
+    }
+    if (count == 2)
+    {
+        return true;
+    }
+    else
+        return false;
+}
