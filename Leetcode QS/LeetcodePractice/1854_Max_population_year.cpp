8454c3e6638fb2e2adb71235d2202622a32313d0
Debarup <87480898+debarup24@users.noreply.github.com>
2025-12-07 12:18:58 +0530

    leetcode

--- Leetcode QS/LeetcodePractice/1854_Max_population_year.cpp
+++ Leetcode QS/LeetcodePractice/1854_Max_population_year.cpp
@@ -1,4 +1,5 @@
-
+#include <bits/stdc++.h>
+using namespace std;
 
 // QS : https://leetcode.com/problems/maximum-population-year/description/ 
 // Explanation : https://chatgpt.com/share/6935206d-457c-8008-98c9-549e3e870a59 
